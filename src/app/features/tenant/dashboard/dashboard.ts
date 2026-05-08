e58ccece03ff4b05e6e04e5d6abe7a370de804da
Juldas RAMDE <juldas1.ramde@orange.com>
2026-05-08 10:07:38 +0000

    feat!: set up navigation in dashboard

--- src/app/features/tenant/dashboard/dashboard.ts
+++ src/app/features/tenant/dashboard/dashboard.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    Button
   ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  styleUrl: './dashboard.scss'
 })
-export class Dashboard {}
+export class Dashboard {
+  private readonly router = inject(Router);
+
+  protected onNavigateTo(query: string) {
+    if (query === 'cat') {
+      void this.router.navigate(['app', 'categories']);
+    } else if (query === 'pro') {
+      void this.router.navigate(['app', 'products']);
+    } else if (query === 'smv') {
+      void this.router.navigate(['app', 'stocks-movements']);
+    }
+  }
+}
